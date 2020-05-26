61ad1ef73b4686f660feb1422600278eeabbc2f2
jeanmichel <jeanmichel@sequans.com>
2020-05-26 18:42:43 +0200

    fix wget

--- scripts/wget.py
+++ scripts/wget.py
@@ -3,20 +3,24 @@ import argparse
 import os
 import codecs
 
+
+def download_file(url, fname):
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(fname, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                # if chunk:
+                f.write(chunk)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="download a file")
     parser.add_argument("url", type=str)
     parser.add_argument("-d", "--dst", type=str, default="")
     args = parser.parse_args()
     if not args.dst:
-        args.dst = os.path.basename(args.url)
-    rsp = requests.get(args.url)
-    txt = "text" in rsp.headers["Content-type"]
-    _args = ("w",) if txt else ("wb",)
-    _kwargs = {"encoding": "utf8"} if txt else {}
-    with open(args.dst, *_args, **_kwargs) as dst:
-        if txt:
-            dst.write(rsp.text)
-        else:
-            for chunk in rsp:
-                dst.write(chunk)
+        args.dst = args.url.split("/")[-1]
+    download_file(args.url, args.dst)
